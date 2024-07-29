52c14d450e71493ef61b3f85cf4179653b2bc200
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-29 19:02:13 +0800

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
 //import { useState } from 'react'
 import './App.css'
 import Board from "./pages/Board.tsx";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       {/*<div className={"bg-amber-300"}>*/}
       {/*    hi*/}
       {/*</div>*/}
         <Board />
-    </>
+    </DndProvider>
   )
 }
 
