9f998b599ff2e1ade8044546247ba55e79d5ee83
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-07-29 13:29:35 +0800

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,29 @@
+import express from "express";
+import mysql from "mysql";
+const app = express();
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test",
+});
+
+app.get("/", (req, res) => {
+    res.json("this is reactsql backend");
+});
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.json(err);
+        }
+        return res.json(data);
+    });
+});
+
+app.listen(8800, ()=>{
+    console.log("connected to ")
+})
