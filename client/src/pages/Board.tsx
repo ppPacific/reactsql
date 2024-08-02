7f640dcdcb47e0ed780dce2a2adef35ec3f4bc84
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-08-02 18:42:57 +0800

    Add files via upload

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 import {ColumnTypes} from "../components/constants/enums.ts";
 import Widgets from "../components/widgets/Widgets.tsx";
 import WidgetContainer from "../components/widgetsContainer/WidgetContainer.tsx";
+import React from "react";
+import DropArea from "../components/droparea/DropArea.tsx";
 
 
 const wids = [
@@ -50,21 +52,51 @@ const Board =()=>{
     //             ))
     //     );
     // };
+    const onDragAdd =(status:string,position:number)=>{
+        console.log(`${activeCard} is dragging to ${status} at ${position}`);
+        if(activeCard===null || activeCard ===undefined) return;
+    }
+    const ref1 = "https://react-email-editor-demo.netlify.app/";
+    const ref2 = "https://github.com/topics/email-editor";
     return (
-            <div className={"min-w-full p-2 flex flex-row gap-3"}>
-                <Column selected={selected} name={SELECTED} outer={"md:w-[48rem] w-[24rem]"}>
+        <>
+            <div className={"p-1 bg-gray-400 text-black text-sm flex gap-4"}>
+                <a href={ref1} target={"_blank"}>reference</a>
+                <a href={ref2} target={"_blank"}>topics github</a>
+            </div>
 
-                    {selected?.map((item,index)=>{
-                        return <WidgetContainer setActiveCard={setActiveCard} key={`${item.type}-${index}`} type={item.type} className={"hover:border-[1px] hover:border-purple-600"}></WidgetContainer>
+            <div className={"min-w-full p-2 flex flex-row gap-3 relative"}>
+                <Column name={SELECTED} outer={"md:w-[48rem] w-[24rem]"}>
+                    <DropArea onDrop={()=>{onDragAdd("selected",0)}}/>
+                    {selected?.map((item, index) => {
+                        return <React.Fragment key={`${item.type}-${index}`}>
+                            <WidgetContainer
+                            setActiveCard={setActiveCard}
+                            index={`selected ${index}`}
+                            type={item.type}
+                            className={"hover:border-[1px] hover:border-purple-600"}>
+                        </WidgetContainer>
+                        <DropArea onDrop={()=>{onDragAdd("selected",index+1)}}/>
+                        </React.Fragment>
                     })}
                     {/*<Button className={"rounded-xl bg-gray-800 p-2 text-white"}>Test Click!</Button>*/}
                 </Column>
                 <Column name={WIDGETS} inner={"flex-col gap-3"}>
-                    {orders?.map((item)=>{
-                        return <Widgets key={item.id} name={item.title} className={"bg-gray-300 rounded-md"}></Widgets>
+                    {orders?.map((item, index) => {
+                        return <Widgets
+                            key={item.id}
+                            setActiveCard={setActiveCard}
+                            index={`widget ${index}`}
+                            name={item.title}
+                            className={"bg-gray-300 rounded-md"}></Widgets>
                     })}
                 </Column>
             </div>
+            <h2>Active Card - {activeCard}</h2>
+            <pre className="text-xs">
+                <p>{JSON.stringify(selected, null, 4)}</p>
+            </pre>
+        </>
     )
 }
 
